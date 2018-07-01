65bab7f9d134d2aacde12c30c57ac76edc54fc24
envincebal <vincentyan1@gmail.com>
2018-07-01 03:05:51 -0700

    Added getUsers method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 import Header from "./components/Header";
+import User from "./components/User";
 import Buttons from "./components/Buttons";
-// ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
-// "https://wind-bow.glitch.me/twitch-api/streams/"
-// "https://wind-bow.glitch.me/twitch-api/channels/"
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"],
+      userInfo: [],
+      online: false,
+      offline: false
+    }
+  }
+
+  componentWillMount() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    const url = "https://wind-bow.glitch.me/twitch-api/streams/";
+
+    this.state.users.forEach(user => {
+      fetch(url + user)
+        .then(data => data.json())
+        .then(response => {
+            this.setState({
+            userInfo: response
+          });
+          console.log(this.state.userInfo);
+
+        })
+    })
+  }
   render() {
     return (
       <div className="App">
--- src/components/User.js
+++ src/components/User.js
@@ -3,9 +3,9 @@ import React from 'react';
 const User = (props) => {
   return (
     <div className="user-container">
-    <div className="img-container">
-      <img className="profile-pic" src="" alt=""/>
-    </div>
+      <div className="img-container">
+        <img className="profile-pic" src="" alt="" />
+      </div>
       <div className="details-container">
         <p className="display-name"></p>
         <p className="status"></p>
